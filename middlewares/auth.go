38af5c6a992c6a5b837b7bda88edcbe94772a5cf
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2019-12-14 20:44:34 +0700

    refactor auth middleware

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
@@ -9,10 +8,6 @@ import (
 
 	"cinemanz/utils"
 )
-
-type key string
-
-const contextKey key = "userInfo"
 
 // IsAuthenticated check token
 func IsAuthenticated(next http.Handler) http.Handler {
@@ -36,20 +31,10 @@ func IsAuthenticated(next http.Handler) http.Handler {
 			return []byte("c0b4d1b4c4"), nil
 		})
 
-		if err != nil {
+		if err != nil || !token.Valid {
 			utils.ResponseError(w, http.StatusBadRequest, err.Error())
 			return
 		}
-
-		claims, ok := token.Claims.(jwt.MapClaims)
-
-		if !ok || !token.Valid {
-			utils.ResponseError(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		ctx := context.WithValue(context.Background(), contextKey, claims)
-		r = r.WithContext(ctx)
 
 		next.ServeHTTP(w, r)
 	})
