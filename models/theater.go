17ab812c8659c77ae9c279a277696549f20367e2
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-01-19 13:44:50 +0700

    improvement: delete unused functions

--- models/theater.go
+++ models/theater.go
@@ -1,13 +1,7 @@
 package models
 
 import (
-	"context"
-
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"cinemanz/databases"
 )
 
 // Theater struct for theater
@@ -16,93 +10,4 @@ type Theater struct {
 	Name      string             `json:"name" bson:"name"`
 	Address   string             `json:"address" bson:"address"`
 	Telephone string             `json:"telephone" bson:"telephone"`
-}
-
-// FetchTheaters return all theater
-func FetchTheaters(skip int64, limit int64) (theaters []Theater, total int64, err error) {
-	options := options.Find().SetSkip(skip).SetLimit(limit)
-
-	cursor, err := databases.Mongo.Collection("theater").Find(
-		context.Background(),
-		bson.M{},
-		options,
-	)
-
-	if err != nil {
-		return nil, 0, err
-	}
-
-	total, err = databases.Mongo.Collection("theater").CountDocuments(
-		context.Background(),
-		bson.M{},
-	)
-
-	if err != nil {
-		return nil, 0, err
-	}
-
-	defer cursor.Close(context.Background())
-
-	for cursor.Next(context.Background()) {
-		var theater Theater
-
-		err := cursor.Decode(&theater)
-
-		if err != nil {
-			return nil, 0, err
-		}
-
-		theaters = append(theaters, theater)
-	}
-
-	return theaters, total, nil
-}
-
-// FetchTheater return a theater
-func FetchTheater(objectID primitive.ObjectID) (theater *Theater, err error) {
-	err = databases.Mongo.Collection("theater").FindOne(
-		context.Background(),
-		bson.M{
-			"_id": objectID,
-		}).Decode(&theater)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return theater, nil
-}
-
-// AddTheater create a theater
-func AddTheater(t Theater) error {
-	var err error
-
-	_, err = databases.Mongo.Collection("theater").InsertOne(context.Background(), t)
-
-	return err
-}
-
-// UpdateTheater update a theater
-func UpdateTheater(objectID primitive.ObjectID, t Theater) error {
-	_, err := databases.Mongo.Collection("theater").UpdateOne(
-		context.Background(),
-		bson.M{"_id": objectID},
-		bson.M{"$set": bson.M{
-			"address":   t.Address,
-			"name":      t.Name,
-			"telephone": t.Telephone,
-		}})
-
-	return err
-}
-
-// DeleteTheater delete a theater
-func DeleteTheater(objectID primitive.ObjectID) error {
-	_, err := databases.Mongo.Collection("theater").DeleteOne(
-		context.Background(),
-		bson.M{
-			"_id": objectID,
-		})
-
-	return err
 }
--- models/user.go
+++ models/user.go
@@ -1,14 +1,8 @@
 package models
 
 import (
-	"context"
-
 	jwt "github.com/dgrijalva/jwt-go"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"golang.org/x/crypto/bcrypt"
-
-	"cinemanz/databases"
 )
 
 // User struct for user
@@ -30,44 +24,4 @@ type MyClaims struct {
 	jwt.StandardClaims
 	Username  string `json:"username" bson:"username"`
 	Privilege string `json:"privilege" bson:"privilege"`
-}
-
-// Register to create new user
-func Register(u User) error {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-
-	if err != nil {
-		return err
-	}
-
-	_, err = databases.Mongo.Collection("user").InsertOne(
-		context.Background(),
-		bson.M{
-			"username":  u.Username,
-			"password":  string(hashedPassword),
-			"privilege": "admin",
-		},
-	)
-
-	return err
-}
-
-// Login return token
-func Login(dataLogin DataLogin) (user *User, err error) {
-	err = databases.Mongo.Collection("user").FindOne(
-		context.Background(),
-		bson.M{"username": dataLogin.Username},
-	).Decode(&user)
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(dataLogin.Password))
-
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
 }
